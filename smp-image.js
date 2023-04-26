8465b4f5df7730af59aed8d41ba88308577096ab
Alexander Volle <alexander.volle@hotmail.com>
2023-04-26 19:02:33 +0200

    Delay DOM manipulation

--- smp-image.js
+++ smp-image.js
@@ -2,26 +2,30 @@
 const linkText = 'Åpne bilde i full størrelse';
 const figureSelector = 'figure.media.image';
 
-// All <figure> elements that contain the classes .media and .image
-const figures = document.querySelectorAll(figureSelector);
+window.addEventListener('load', () => {
+    window.setTimeout(() => {
+        // All <figure> elements that contain the classes .media and .image
+        const figures = document.querySelectorAll(figureSelector);
 
-figures.forEach(figure=> {
-    // Find the image the figure contains and get the URL without search params ?hey=1 etc.
-    const image = figure.querySelector('img');
-    const url = new URL(image.src);
-    const abbreviatedUrl = url.origin + url.pathname;
+        figures.forEach(figure=> {
+            // Find the image the figure contains and get the URL without search params ?hey=1 etc.
+            const image = figure.querySelector('img');
+            const url = new URL(image.src);
+            const abbreviatedUrl = url.origin + url.pathname;
 
-    // Create a link with text and the href as formatted in abbreviatedUrl
-    const link = document.createElement('a');
-    link.href = abbreviatedUrl;
-    link.target = '_blank';
-    link.innerText = linkText;
+            // Create a link with text and the href as formatted in abbreviatedUrl
+            const link = document.createElement('a');
+            link.href = abbreviatedUrl;
+            link.target = '_blank';
+            link.innerText = linkText;
 
-    // Create a link container to space around the link
-    const linkContainer = document.createElement('div');
-    linkContainer.style.margin = '1rem';
-    linkContainer.appendChild(link);
+            // Create a link container to space around the link
+            const linkContainer = document.createElement('div');
+            linkContainer.style.margin = '1rem';
+            linkContainer.appendChild(link);
 
-    // Add link and link container after the figure
-    figure.after(linkContainer);
-});
+            // Add link and link container after the figure
+            figure.after(linkContainer);
+        });
+    }, 500)
+})
